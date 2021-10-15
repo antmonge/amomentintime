c27538532632a7a497ab9d6320aa9012337ab2a6
antmonge <91699751+antmonge@users.noreply.github.com>
2021-10-14 23:19:43 -0400

    Update index.js

--- server/index.js
+++ server/index.js
@@ -9,25 +9,21 @@ app.use(bodyParser.json())
 app.use(express.static(path.resolve(__dirname, '../client/build')));
 
 app.post('/api', (req, res) => {
-  res.json({message: "Test1"})
   console.log(`Start`)
   console.log(req.body)
   let dataToSend
 //  let largeDataSet = []
   // spawn new child process to call the python script
   const python = spawn('python', ['server/test2.py', req.body[1], req.body[3]])
-  res.json({message: "Test2"})
 //  console.log('Start')
   // collect data from script
   python.stdout.on('data', function (data) {
-    res.json({message: "Test3"})
     console.log(`Pipe data from python script ...`)
     dataToSend =  data;
 //    //largeDataSet.push(data)
   })
 
   python.stderr.on('data', function (data) {
-    res.json({message: "Test4"})
     console.log(`Python script error ${data}`)
     dataToSend =  data;
 //    //largeDataSet.push(data)
@@ -37,7 +33,7 @@ app.post('/api', (req, res) => {
   python.on('close', (code) => {
     console.log(`child process close all stdio with code ${code}`)
     //send data to browser
-    //res.json({message: "Test1"})
+    res.json({message: "Test1"})
   })
 });
 
