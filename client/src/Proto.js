5561314b998160e0230e5ce3605f718a3f52d523
antmonge <91699751+antmonge@users.noreply.github.com>
2021-11-04 16:28:53 -0400

    Proto6

--- client/src/Proto.js
+++ client/src/Proto.js
@@ -17,22 +17,22 @@ import proto6Snowstorm from './proto6/proto6-Snowstorm-test.svg'
 import proto6Fishing from './proto6/proto6-Fishing-test.svg'
 import proto6XmasHoliday from './proto6/proto6-XmasHoliday-test.svg'
 import proto6SundayWithFamily from './proto6/proto6-SundayWithFamily-test.svg'
-//import proto6DownDay from './proto6/proto6-DownDay.svg'
-//import proto6NightOut from './proto6/proto6-NightOut.svg'
-//import proto6DinnerParty from './proto6/proto6-DinnerParty.svg'
-//import proto6SeasonalActivity from './proto6/proto6-SeasonalActivity.svg'
-//import proto6Shopping from './proto6/proto6-Shopping.svg'
-//import proto6GarageSaleing from './proto6/proto6-GarageSaleing.svg'
-//import proto6TravelDay from './proto6/proto6-TravelDay.svg'
-//import proto6KidMilestone from './proto6/proto6-KidMilestone.svg'
-//import proto6PlayInSportsGame from './proto6/proto6-PlayInSportsGame.svg'
-//import proto6Date from './proto6/proto6-Date.svg'
-//import proto6MeetSomeoneNew from './proto6/proto6-MeetSomeoneNew.svg'
-//import proto6RandomActKindness from './proto6/proto6-RandomActKindness.svg'
+import proto6DownDay from './proto6/proto6-DownDay-test.svg'
+import proto6NightOut from './proto6/proto6-NightOut-test.svg'
+import proto6DinnerParty from './proto6/proto6-DinnerParty-test.svg'
+import proto6SeasonalActivity from './proto6/proto6-SeasonalActivity-test.svg'
+import proto6Shopping from './proto6/proto6-Shopping-test.svg'
+import proto6GarageSaleing from './proto6/proto6-GarageSaleing-test.svg'
+import proto6TravelDay from './proto6/proto6-TravelDay-test.svg'
+import proto6KidMilestone from './proto6/proto6-KidMilestone-test.svg'
+import proto6PlayInSportsGame from './proto6/proto6-PlayInSportsGame-test.svg'
+import proto6Date from './proto6/proto6-Date-test.svg'
+import proto6MeetSomeoneNew from './proto6/proto6-MeetSomeoneNew-test.svg'
+import proto6RandomActKindness from './proto6/proto6-RandomActKindness-test.svg'
 import './App.css';
 
 function Proto() {
-  const protoLabel = ['Crazy Productive Day', 'Baseball Game', 'Graduation Day', 'Kids Graduation', 'Wedding', 'Kids Wedding', 'Funeral', 'Day At Beach', 'High School Party', 'Relationship Breakup', 'Scammed', 'Fight With Friends', 'Lazy Summer Day', 'Rainy Day', 'Snowstorm', 'Fishing', 'Xmas Holiday', 'Sunday With Family'];
+  const protoLabel = ['Crazy Productive Day', 'Baseball Game', 'Graduation Day', 'Kids Graduation', 'Wedding', 'Kids Wedding', 'Funeral', 'Day At Beach', 'High School Party', 'Relationship Breakup', 'Scammed', 'Fight With Friends', 'Lazy Summer Day', 'Rainy Day', 'Snowstorm', 'Fishing', 'Xmas Holiday', 'Sunday With Family', 'Down Day', 'Night Out', 'Dinner Party', 'Seasonal Activity', 'Shopping', 'Garage Saleing', 'Travel Day', 'Kid Milestone', 'Play In Sports Game', 'Date', 'Meet Someone New', 'Random Act Kindness'];
   return (
     <div id="proto">
       <table>
@@ -86,6 +86,38 @@ function Proto() {
           <td className="ProtoCap">{protoLabel[15]}</td>
           <td className="ProtoCap">{protoLabel[16]}</td>
           <td className="ProtoCap">{protoLabel[17]}</td>
+        </tr>
+        <tr>
+          <td><img src={proto6DownDay} className="ProtoImg" alt={protoLabel[18]} /></td>
+          <td><img src={proto6NightOut} className="ProtoImg" alt={protoLabel[19]} /></td>
+          <td><img src={proto6DinnerParty} className="ProtoImg" alt={protoLabel[20]} /></td>
+          <td><img src={proto6SeasonalActivity} className="ProtoImg" alt={protoLabel[21]} /></td>
+          <td><img src={proto6Shopping} className="ProtoImg" alt={protoLabel[22]} /></td>
+          <td><img src={proto6GarageSaleing} className="ProtoImg" alt={protoLabel[23]} /></td>
+        </tr>
+        <tr>
+          <td className="ProtoCap">{protoLabel[18]}</td>
+          <td className="ProtoCap">{protoLabel[19]}</td>
+          <td className="ProtoCap">{protoLabel[20]}</td>
+          <td className="ProtoCap">{protoLabel[21]}</td>
+          <td className="ProtoCap">{protoLabel[22]}</td>
+          <td className="ProtoCap">{protoLabel[23]}</td>
+        </tr>
+        <tr>
+          <td><img src={proto6TravelDay} className="ProtoImg" alt={protoLabel[24]} /></td>
+          <td><img src={proto6KidMilestone} className="ProtoImg" alt={protoLabel[25]} /></td>
+          <td><img src={proto6PlayInSportsGame} className="ProtoImg" alt={protoLabel[26]} /></td>
+          <td><img src={proto6Date} className="ProtoImg" alt={protoLabel[27]} /></td>
+          <td><img src={proto6MeetSomeoneNew} className="ProtoImg" alt={protoLabel[28]} /></td>
+          <td><img src={proto6RandomActKindness} className="ProtoImg" alt={protoLabel[29]} /></td>
+        </tr>
+        <tr>
+          <td className="ProtoCap">{protoLabel[24]}</td>
+          <td className="ProtoCap">{protoLabel[25]}</td>
+          <td className="ProtoCap">{protoLabel[26]}</td>
+          <td className="ProtoCap">{protoLabel[27]}</td>
+          <td className="ProtoCap">{protoLabel[28]}</td>
+          <td className="ProtoCap">{protoLabel[29]}</td>
         </tr>
       </table>
     </div>
