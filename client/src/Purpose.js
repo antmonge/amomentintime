d2d33478974c7db59816bcbcbcdb22a4c06106ed
antmonge <91699751+antmonge@users.noreply.github.com>
2021-11-30 23:25:59 -0500

    Purpose

--- client/src/Purpose.js
+++ client/src/Purpose.js
@@ -8,45 +8,46 @@ export default class Purpose extends React.Component {
     return (
       <React.Fragment>
       <div className="PurposeBodyA">
+      <div className="PurposeHeader"><p>Vision for A Moment in Time</p></div>
+      <div className="PurposeBody"><p>We have created a token that acts as a keepsake, keeping the memories of your special moment alive.  As a token protected and stored using blockchain technology you will have access into a window of your past by dusting off your keepsake, connecting with your emotions and honoring what made that moment meaningful for you.  The token will provide access to a community that will allow you to share your moment with likeminded community members and will provide you the opportunity to listen and offer support to your fellow community members.  While the experiences will be personal, you will be able to connect with community members who share similar experiences.  The community will establish a library of curated resources to help cope with all situations life can present, develop skills to help those you care about navigate lifes circumstances, and a framework to share and get feedback for lifes most challenging situations</p></div>
+      </div>
+      <div className="PurposeBodyB">
+      <div className="PurposeHeader"><p>Why did We Launch?</p></div>
+      <div className="PurposeBody"><p>1. Our memories are never quite as vivid and clear as we wish them to be.  We think back to the emotions, the people, the places and maybe we even try to revisit and recreate, but those moments are just that - moments in time, impossible to recreate no matter how hard we try.  Blockchain and the immutable nature of the tokens stored on blockchain could help us capture what makes our moments special and store those aspects for eternity.</p>  <p>2. Most NFT art is the brainchild of the artist with little input from those that will own and appreciate the art.  A Moment in Time gives the holder the chance to influence the piece of art they will own.  Couple the creative control with a .01 eth mint price and an open ended supply making A Moment in Time a project focused on the community.  Our intent is to create a project that is accessible to NFT newcomers as an entry point NFT project yet appealing to those already in the NFT community by simplifying the process - no gas wars, no blind reveals, no need to research rarity, no sweating floor prices .  This is a token for you by you with a focus on personalization, community, and growth.</p>  <p>3. Every single one of us struggles with emotional issues at different times in our lives.  Mental Health and Well Being have become less taboo and have received well deserved attention.  At the same time there are way too many that throw the terms out there and then under deliver never really offrering up meaningful support for those that need it most.</p></div>
+      </div>
+      <div className="PurposeBodyA">
+      <div className="PurposeHeader"><p>Why Mint?</p></div>
+      <div className="PurposeBody"><p>The world is fascinating, it is so small yet massive.  NFTs and digital communities have connected us in ways that many have not been exposed to before.  People from vastly differnet regions connecting with shared experiences and unique ways of exploring lifes moments.  Minting A Moment in Time provides you with a keepsake that can be tapped into bringing you back to your moment frozen in time on the blockchain, but it also connects you with a community that can help you honor your moment.  Too often, we are hesitant, ashamed, guilty, sharing our moments with those that we are close with.  A Moment in Time provides you with a safe community to share our Moments with, express ineterst and support in others moments, and tap into a reputable set of resources to make us more supportive of others jpurney thrugh life.</p></div>
+      </div>
+      <div className="PurposeBodyB">
+      <div className="PurposeHeader"><p>Mint and Secondary Sales</p></div>
+      <div className="PurposeBody"><p>For each edition the mint funds will be allocated 25% to the founders, 25% to the artist, and 50% to the community.  For Secondary Sales there will be a 5% royalty with the same allocation split as the mint.</p></div>
+      </div>
+      <div className="PurposeBodyA">
+      <div className="PurposeHeader"><p>Who are We?</p></div>
+      <div className="PurposeBody"><p>antmonge</p>
+      <p>CarBar</p></div>
+      </div>
+      <div className="PurposeBodyB">
+      <div className="PurposeHeader"><p>The Art</p></div>
+      <div className="PurposeBody"><p>Neither of our founders has an artistic background.  However, both are logical and analytical in nature.  The beauty of generative art is that it allows those that may not have natural paint brush abilities to let their inner artists shine through in a digital sense by using their structured logic skills to express artistic expressions.  The generative art created here layers emotions interpreted through vector images and a spectrum of colors.  The art was inspired by the pioneered efforts of Rudy Mancuso to translate audio senses to visual senses in the form of a painting.</p></div>
+      </div>
+      <div className="PurposeBodyA">
+      <div className="PurposeHeader"><p>The Community</p></div>
+      <div className="PurposeBody"><p>As a project we intend to be a reflection of our community.  A collection of people navigating through the life and honoring the special moments that define us all.  These are the moments that build and break us.  This community will be a place for us to go to share our moments.  We will be there to support each other out of respect and compassion whether the share is deep and emotional or light and fun there will be a space for you to share, support, and contribute.</p></div>
+      </div>
+      <div className="PurposeBodyB">
+      <div className="PurposeHeader"><p>The Resources</p></div>
+      <div className="PurposeBody"><p>A library of resources will be built through the community based on feedback and conrtibutions from community members.  Through sharing, volunteering, and funds from the community wallet we will build a library of resources, hosted events, and sessions focused on learning and growth.  The resources will help community members appreciate how special all of our moments are, learn to navigate lifes moments, and build the tools needed to support others in their moments.</p></div>
+      </div>
+      <div className="PurposeBodyA">
       <div className="PurposeHeader"><p>Twitter Bio/Marketing Taglines</p></div>
       <div className="PurposeBody"><p>An NFT project that honors meaningful moments in your life by capturing your emotions and stamping them on blockchain solidifying your memories and supporting your growth through a community focused on being there to help you on the journey of life.</p>
       <p>A token that captures what makes those moments in your life special.  Lock in your memories and join a community where members help each other navigate the joruney of life</p></div>
       </div>
       <div className="PurposeBodyB">
-      <div className="PurposeHeader"><p>Vision for A Moment in Time</p></div>
-      <div className="PurposeBody"><p>We have created a token that acts as a keepsake, keeping the memories of your special moment alive.  As a token protected and stored using blockchain technology you will have access into a window of your past by dusting off your keepsake, connecting with your emotions and honoring what made that moment meaningful for you.  The token will provide access to a community that will allow you to share your moment with likeminded community members and will provide you the opportunity to listen and provide support for your fellow community members.  While the experiences will be personal you will be able to connect with community members with similar experiences.  The community will establish a library of curated resources to help cope with all situations life can present to you, develop skills to help those you care about navigate lifes circumstances, and a framework to share and get feedback for the most challenging situations</p></div>
-      </div>
-      <div className="PurposeBodyA">
-      <div className="PurposeHeader"><p>Why did We Launch?</p></div>
-      <div className="PurposeBody"><p>1. Our memories are never quite as vivid and clear as we wish them to be.  We think back about the emotions, the people, the places and maybe we even try to revisit and recreate, but those moments are just that - moments in time, impossible to recreate no matter how hard we try.  Blockchain and the immutable nature of the tokens stored on blockchain could help us capture what makes our moments special and store those aspects for eternity.</p>  <p>2. Most NFT art is the brainchild of the artist with little input from those that will own and appreciate the art.  A Moment in Time gives the holder the chance to influence the piece of art they will own.  Couple the creative control with a .01 eth mint price and an open ended supply making A Moment in Time a project focused on the community.  Our intent is to create a project that is accessible to NFT newcomers yet appealing to those already in the NFT community by simplifying the process - no gas wars, no blind reveals, no need to research rarity.  This is a token for you by you.</p>  <p>3. Every single one of us struggles with emotional issues at different times in our lives.  Mental Health and Well Being have become less taboo and have received well deserved attention.  At the same time there are way too many that throw the terms out there and then under deliver never really offrering up meaningful support for those that need it most.</p></div>
-      </div>
-      <div className="PurposeBodyB">
-      <div className="PurposeHeader"><p>Why Mint?</p></div>
-      <div className="PurposeBody"><p>The world is fascinating, it is so small yet massive.  NFTs and digital communities have connected us in ways that many have not been exposed to before.  People from vastly differnet regions connecting with shared experiences and unique ways of exploring lifes moments.  Minting A Moment in Time provides you with a keepsake that can be tapped into to bring you back to your moment, but it also connects you with a community that can help you honor your moment.  Too often, we are hesitant, ashamed, guilty, sharing our moments with those that we are close with.  A Moment in Time provides you with a safe community to share our Moments with, express ineterst and support in others moments, and tap into a reputable set of resources to make us more supportive of others jpurney thrugh life.</p></div>
-      </div>
-      <div className="PurposeBodyA">
-      <div className="PurposeHeader"><p>Mint and Secondary Sales</p></div>
-      <div className="PurposeBody"><p>For each edition the mint funds will be allocated 25% to the founders, 25% to the artist, and 50% to the community.  For Secondary Sales there will be a 5% royalty with the same allocation split as the mint.</p></div>
-      </div>
-      <div className="PurposeBodyB">
-      <div className="PurposeHeader"><p>Who are We?</p></div>
-      <div className="PurposeBody"><p>antmonge</p>
-      <p>CarBar</p></div>
-      </div>
-      <div className="PurposeBodyA">
-      <div className="PurposeHeader"><p>The Art</p></div>
-      <div className="PurposeBody"><p>Neither of our founders has an artistic background.  However, both are logical and analytical in nature.  The beauty of generative art is that it allows those that may not have natural paint brush abilities to let their inner artists shine through in a digital sense by using their structured logic skills to express artistic expressions.  The generative art created here layers emotions interpreted through vector images and a spectrum of colors.  The art was inspired by the pioneered efforts of Rudy Mancuso to translate audio senses to visual senses in the form of a painting.</p></div>
-      </div>
-      <div className="PurposeBodyB">
-      <div className="PurposeHeader"><p>The Community</p></div>
-      <div className="PurposeBody"><p></p></div>
-      </div>
-      <div className="PurposeBodyA">
-      <div className="PurposeHeader"><p>The Resources</p></div>
-      <div className="PurposeBody"><p></p></div>
-      </div>
-      <div className="PurposeBodyB">
-      <div className="PurposeHeader"><p>Near Term, Mid Term, Long Term</p></div>
+      <div className="PurposeHeader"><p>Plans</p></div>
+      <div className="PurposeBody"><p>1. Near Term - </p><p>2. Mid Term - </p><p>3. Long Term - </p></div>
       </div>
       </React.Fragment>
     );
